49ddf2a3103f990cfae319c1f423376b4fb64bb2
Tzu Hsuan Huang(Alyssa) <117048389+allyer88@users.noreply.github.com>
2024-04-08 14:05:22 -0700

    init, default_init test runable

--- main.cpp
+++ main.cpp
@@ -16,12 +16,15 @@ private:
 public:
     //1 means ready
     //0 means block
-    //-1 means running
+    //-1 means new(not yet created)
     PCB(){
-        this->state = 1;
+        this->state = -1;
         this->parent = -1;//if it is 
         this->priority= -1;
     };
+    void setState(int state){
+        this->state = state;
+    }
     void setParent(int parent){
         this->parent = parent;
     }
@@ -29,7 +32,7 @@ public:
         this->priority=priority;
     }
     void reset(){
-        this->state = 1;
+        this->state = -1;
         this->parent = -1;
         this->priority= -1;
         this->children.clear();
@@ -50,6 +53,7 @@ public:
     void setInventory(int inventory){
         this->inventory = inventory;
     }
+    //erase the data
     void reset(){
         this->state = 1;
         this->inventory = 0;
@@ -73,11 +77,26 @@ class Manager{
     PCB pcb[16];
     RCB rcb[4];
     deque<int>* RL;
+    //current running process
+    int running;
+    //index of process that is not yet created
+    int indexProcess;
     Manager(){};
     ~Manager(){
         delete[] RL;
     }
-    void create();
+    void create(int j){
+        //allocate new PCB[j]
+        //state = ready
+        pcb[j].setState(1);
+        //insert j into list of children of i
+        
+        //parent = i
+        //children = NULL 
+        //resources = NULL 
+        //insert j into RL
+        //display: “process j created”
+    }
     void destroy();
     void request();
     void release();
@@ -93,8 +112,10 @@ class Manager{
         }
         delete[] RL;
     }
-    void init(int level, int r1, int r2, int r3, int r4){
-        reset();
+    void init(bool first, int level, int r1, int r2, int r3, int r4){
+        //erase all previous data
+        if(!first) reset();
+        //set value
         pcb[0].setPriority(0);
         rcb[0].setInventory(r1);
         rcb[1].setInventory(r2);
@@ -102,11 +123,14 @@ class Manager{
         rcb[3].setInventory(r4);
         RL = new deque<int>[level];
         RL[0].push_back(0);
+        running = 0;
+        indexProcess = 1;
+        //print the current running process
+        cout<<"0"<<endl;
     };
-    void init_default(){
-        init(3, 1, 1, 2, 3);
+    void init_default(bool first){
+        init(first, 3, 1, 1, 2, 3);
     }
-
 };
 
 int main(){
@@ -114,12 +138,15 @@ int main(){
     vector<string> argms;
     //get the command from users
     bool isStopped = false;
+    Manager manager;
+    int first = true;
     while(!isStopped){
-        cout<< '> '<< endl;
+        cout<< "> ";
         getline(cin, command);
         //get each word from command
         getCommands(argms, command);
         int argmsize = argms.size();
+        //cout<<"argument size:" <<argmsize<<endl;
         if(argmsize<=6){
             if(argmsize == 4 || argmsize == 5) {
                 argms.clear();
@@ -127,14 +154,21 @@ int main(){
                 continue;
             }
             if(argms[0]=="in" && argmsize==6){
+                //stoi turn string to int
+                manager.init(first, stoi(argms[1]),stoi(argms[2]),stoi(argms[3]),stoi(argms[4]),stoi(argms[5]));
+                first = false;
+            }else if(argms[0]=="id" && argmsize==1){
+                manager.init_default(first);
+                first = false;
+            }else if(argms[0]=="stop"){
+                isStopped=true;
             }
-
         }else{
             argms.clear();
             cout<<"Invalid command\n";
             continue;
         }
+        argms.clear();
     }
-    
-
+    return 0;
 }
