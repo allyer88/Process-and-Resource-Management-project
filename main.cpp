a39f2a78cd4da3c7cb25955794d9300e95f81028
Tzu Hsuan Huang(Alyssa) <117048389+allyer88@users.noreply.github.com>
2024-04-08 18:39:09 -0700

    release() runnable

--- main.cpp
+++ main.cpp
@@ -31,6 +31,9 @@ public:
     void setPriority(int priority){
         this->priority=priority;
     }
+    int getPriority(){
+        return this->priority;
+    }
     void reset(){
         this->state = -1;
         this->parent = -1;
@@ -46,6 +49,14 @@ public:
     void addResource(int rcb){
         this->resources.push_back(rcb);
     }
+    void rmResource(int rs){
+        for(int i=0;i<resources.size();i++){
+            if(resources[i]==rs){
+                resources.erase(resources.begin()+i);
+                break;
+            }
+        }
+    }
 };
 struct PR{
     int process;
@@ -60,11 +71,11 @@ private:
     int state;
     int freeInventory;
     int UsingInventory;
+public:
     //which process is holding how many recourses
     deque<PR> resources;
     //which process is waiting for how many resources
     deque<PR> waitlist;
-public:
     RCB(){
         //1 free
         //0 allocated
@@ -72,20 +83,58 @@ public:
         this->freeInventory = 0;
         this->UsingInventory = 0;
     };
-    void setInventory(int inventory){
+    int getState(){
+        return this->state;
+    }
+    void setState(int state){
+        this->state=state;
+    }
+    void setFreeInventory(int inventory){
         this->freeInventory = inventory;
+    }
+    void setUsingInventory(int inventory){
+        this->UsingInventory = inventory;
+    }
+    void getResource(PR pr){
+        //this process can get resource
+        this->freeInventory -= pr.resource;
+        this->UsingInventory += pr.resource;
+        this->resources.push_back(pr);
+        if(this->freeInventory==0){
+            this->state = 0;
+        }
     }
     //if no error, it can request
     bool request(int process,int units){
         PR pr(process, units);
         if(canRequest(units)){
-            this->freeInventory -= units;
-            this->UsingInventory += units;
-            this->resources.push_back(pr);
+            getResource(pr);
             return true;
         }
         addWaitlist(pr);  
         return false;
+    }
+    void release(int process, int releasingunits){
+        int index = 0;
+        for(int i=0;i<resources.size();i++){
+            if(this->resources[i].process == process){
+                index = i;
+                break;
+            }
+        }
+        int holdingunits = resources[index].resource;
+        //check to release the whole or some resources
+        if(holdingunits==releasingunits){
+            this->resources.erase(resources.begin()+index);
+        }else{
+            this->resources[index].resource-=releasingunits;
+        }
+        freeInventory+=releasingunits;
+        UsingInventory-=releasingunits;
+        //state check
+        if(this->state==0){
+            this->state=1;
+        }
     }
     bool canRequest(int request){
         return this->freeInventory>=request;
@@ -105,6 +154,9 @@ public:
     }
     void addWaitlist(PR pr){
         this->waitlist.push_back(pr);
+    }
+    int getWaitlistSize(){
+        return this->waitlist.size();
     }
 };
 
@@ -156,13 +208,15 @@ public:
         int levelrunning = 0;
         for(int i=1;i<totalLevel;i++){
             if(RL[i].size()>0){
-                priProcess =  RL[i].front();
+                priProcess = RL[i].front();
                 levelrunning = i;
                 break;
             }
         }
-        this->runningProc = priProcess;
-        this->runningLevel = levelrunning;
+        if(runningProc!=priProcess){
+            this->runningProc = priProcess;
+            this->runningLevel = levelrunning;
+        }
         //display: “process j runningProc”
         cout<<this->runningProc<<" ";
     }
@@ -198,7 +252,63 @@ public:
             cout<<this->runningProc<<" ";
         }
     }
-    void release();
+    void release(int r, int releasingunits){
+        
+        int index = 0;
+        for(int i=0;i<rcb[r].resources.size();i++){
+            if(this->rcb[r].resources[i].process == runningProc){
+                index = i;
+                break;
+            }
+        }
+        int holdingunits = rcb[r].resources[index].resource;
+        if(releasingunits>holdingunits){
+            cout<<"-1 ";
+            return;
+        }
+        //check to release the whole or some resources
+        if(holdingunits==releasingunits){
+            //remove (r, k) from rcb[r].resources
+            this->rcb[r].resources.erase(rcb[r].resources.begin()+index);
+            //remove resource from resourse list in running process
+            pcb[runningProc].rmResource(r);
+        }else{
+            this->rcb[r].resources[index].resource-=releasingunits;
+        }
+        //r.free = r.free + k
+        rcb[r].setFreeInventory(rcb[r].getFreeInventory()+releasingunits);
+        rcb[r].setUsingInventory(rcb[r].getUsingInventory()-releasingunits);
+        //state check
+        if(rcb[r].getState()==0){
+            rcb[r].setState(1);
+        }
+        bool canRequest = true;
+        int pr;
+        int rs;
+            //while (r.waitlist != empty and r.free > 0)
+        while(rcb[r].waitlist.size()>0 && canRequest){
+            //get next (j, k) from r.waitlist
+            pr = rcb[r].waitlist.front().process;
+            rs = rcb[r].waitlist.front().resource;
+            //if there are enough resource for waiting process if (r.free >= k)
+            if(rs<=rcb[r].getFreeInventory()){
+                //r.free = r.free - k
+                rcb[r].getResource(PR(pr,rs));
+                //insert r into j.resources
+                pcb[pr].addResource(r);
+                //j.state = ready
+                pcb[pr].setState(1);
+                //remove (j, k) from r.waitlist
+                rcb[r].waitlist.pop_front();
+                //insert j into RL
+                int level = pcb[pr].getPriority();
+                RL[level].push_back(pr);
+            }else{
+                canRequest=false;
+            }
+        }
+        scheduler();
+    }
     
     void timeout(){
         //move process i from head of RL to end of RL of that level
@@ -226,10 +336,10 @@ public:
         if(!first) reset();
         //set value
         pcb[0].setPriority(0);
-        rcb[0].setInventory(r1);
-        rcb[1].setInventory(r2);
-        rcb[2].setInventory(r3);
-        rcb[3].setInventory(r4);
+        rcb[0].setFreeInventory(r1);
+        rcb[1].setFreeInventory(r2);
+        rcb[2].setFreeInventory(r3);
+        rcb[3].setFreeInventory(r4);
         RL = new deque<int>[level];
         RL[0].push_back(0);
         runningProc = 0;
@@ -289,7 +399,14 @@ int main(){
                     continue;
                 }
                 manager.request(r,k);
-
+            }else if(argms[0]=="rl" && argmsize==3){
+                int r = stoi(argms[1]);
+                int k = stoi(argms[2]);
+                if(r<0 || r>manager.getTotalLevel()){
+                    cout<<"-1 ";
+                    continue;
+                }
+                manager.release(r,k);
             }
             else if(argms[0]=="stop"){
                 isStopped=true;
